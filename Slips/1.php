73978f4bd044b28058786fc817796987fe98fad6
pratimak121 <pratimakokane1999@gmail.com>
2025-03-28 11:02:15 -0700

    Update 1.php

--- Slips/1.php
+++ Slips/1.php
@@ -1,1 +1,21 @@
+//.Write a PHP script to keep track of number of times the web
+  page has been accessed (Use Session Tracking).
+<?php
+//start the session
+session_start();
+//check if the count' session variable is set
+if(isset($_SESSION['count']))
+{
+    $_SESSION['count']++ ;
+}
+else
+{
+    $_SESSION['count'] = 1;
+}
 
+echo "This page has been accessed " . $_SESSION['count'] ." times.";
+
+?>
+
+//localhost refused to connect. (ensure apache start or not)
+// The requested URL was not found on this server.(ensure file is created in htdocs folder)
